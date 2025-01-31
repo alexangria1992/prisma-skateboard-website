0c928d89b229390c70ec323d9b0e878109e9a131
Alexandre Angrignon <alex040892@gmail.com>
2025-01-31 14:38:36 +1300

    Styling the skateboard

--- src/slices/ProductGrid/index.tsx
+++ src/slices/ProductGrid/index.tsx
@@ -1,5 +1,12 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { Bounded } from '@/components/Bounded';
+import { Heading } from '@/components/Heading';
+import { Content, isFilled } from '@prismicio/client';
+import {
+  PrismicRichText,
+  PrismicText,
+  SliceComponentProps,
+} from '@prismicio/react';
+import { SkateboardProduct } from './SkateboardProduct';
 
 /**
  * Props for `ProductGrid`.
@@ -9,15 +16,29 @@ export type ProductGridProps = SliceComponentProps<Content.ProductGridSlice>;
 /**
  * Component for "ProductGrid" Slices.
  */
-const ProductGrid = ({ slice }: ProductGridProps): JSX.Element => {
+const ProductGrid = ({ slice }: ProductGridProps): React.JSX.Element => {
+  console.log(slice.primary.product[0]);
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className='bg-texture bg-brand-gray'
     >
-      Placeholder component for product_grid (variation: {slice.variation})
-      Slices
-    </section>
+      <Heading className='text-center ~mb-4/6' as='h2'>
+        <PrismicText field={slice.primary.heading} />
+      </Heading>
+      <div className='text-center ~mb-6/10'>
+        <PrismicRichText field={slice.primary.body} />
+      </div>
+      <div className='grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4'>
+        {slice.primary.product.map(
+          ({ skateboard }) =>
+            isFilled.contentRelationship(skateboard) && (
+              <SkateboardProduct key={skateboard.id} id={skateboard.id} />
+            )
+        )}
+      </div>
+    </Bounded>
   );
 };
 
